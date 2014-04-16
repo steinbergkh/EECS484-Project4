60ca6f5b20c06ba5b5b2c7a5d3b60a48d6fc585d
Katie Steinberg <csteinb@umich.edu>
2014-04-15 21:31:42 -0400

    start select.cpp

--- select.cpp
+++ select.cpp
@@ -1,6 +1,7 @@
 #include "catalog.h"
 #include "query.h"
 #include "index.h"
+#include "string.h"
 
 /*
  * Selects records from the specified relation.
@@ -12,11 +13,20 @@
 Status Operators::Select(const string & result,      // name of the output relation
 	                 const int projCnt,          // number of attributes in the projection
 		         const attrInfo projNames[], // the list of projection attributes
-		         const attrInfo *attr,       // attribute used inthe selection predicate 
+		         const attrInfo *attr,       // attribute used inthe selection predicate
 		         const Operator op,         // predicate operation
 		         const void *attrValue)     // literal value in the predicate
 {
+   Status status;
 
-return OK;
+   // initialize result relation
+   RelDesc resultRel;
+   strcpy(resultRel.relName, result);
+   resultRel.attrCnt = projCnt // projected attributes are the
+                              // attributes of the result relation
+   resultRel.indexCnt = 0; // no indexes on the result relation b/c it's temp
+
+
+
+   return OK;
 }
-
