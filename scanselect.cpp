18aa0cb6805a78e7809ad5dea3ed270e26e6c12c
Chris Simon <chrsimon@umich.edu>
2014-04-17 16:51:06 -0400

    selectscan.cpp - first iteration

--- scanselect.cpp
+++ scanselect.cpp
@@ -2,7 +2,7 @@
 #include "query.h"
 #include "index.h"
 
-/* 
+/*
  * A simple scan select using a heap file scan
  */
 
@@ -15,8 +15,28 @@ Status Operators::ScanSelect(const string& result,       // Name of the output r
                              const int reclen)           // Length of a tuple in the result relation
 {
   cout << "Algorithm: File Scan" << endl;
-  
+
   /* Your solution goes here */
+  Status status;
+  Heapfile(result, status);
+
+  HeapFileScan *heapFileScan;
+  if(attrDesc){ //where clause exists -> gotta filter
+    Datatype projAttrType = (Datatype)attrDesc->attrType;
+    char *projAttrVal = (char*)attrValue;  //do we have to static cast this?
+    heapFileScan = new HeapFileScan(attrDesc->relname, attrDesc->attrOffset, attrDesc->attrLen,
+                                    projAttrType, projAttrVal, op, status);
+  }
+  else { //no where clause
+    heapFileScan = new HeapFileScan(projNames[0].relname, status);
+    heapFileScan->startScan(-1, //if offset < 0  return bad scan param error
+                            0, //if length < 1  return bad scan param error
+                            (DataType)0, //do we have to static cast this?
+                            NULL, //no filtering requested, should make function return okay
+                            (Operator)0);
+  }
+
+  // TODO check status here!
 
   return OK;
 }
