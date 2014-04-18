6ef0cd85f8850bddd0c4cf5db03415b7c5579815
Katie Steinberg <csteinb@umich.edu>
2014-04-17 20:38:20 -0400

    oops forgot to include stuff

--- indexselect.cpp
+++ indexselect.cpp
@@ -1,6 +1,9 @@
 #include "catalog.h"
 #include "query.h"
 #include "index.h"
+#include "string.h"
+#include "stdlib.h"
+
 Status Operators::IndexSelect(const string& result,       // Name of the output relation
                               const int projCnt,          // Number of attributes in the projection
                               const AttrDesc projNames[], // Projection list (as AttrDesc)
